46384314392dcf837aede95f0d988c9bfc44528f
Muhammad <ranamuhmmad021@gmail.com>
2024-10-08 10:41:26 +0500

    undo changes

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -29,7 +29,7 @@ export class AddPatientComponent implements OnInit {
     });
   }
 
-  onSubmit(): void { // On Submit
+  onSubmit(): void {
     if (this.addPatientForm.valid) {
       const newPatient: IPatient = {
         id: Date.now(), // Temporary ID generation (should be handled by the backend in real apps)
